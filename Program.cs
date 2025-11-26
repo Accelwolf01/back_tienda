402913019922689ed03da20bf5e389af85f2a1c5
Accelwolf01 <accelwolf01@gmail.com>
2025-11-26 15:58:12 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200","https://front-tienda-qcfy.onrender.com")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
