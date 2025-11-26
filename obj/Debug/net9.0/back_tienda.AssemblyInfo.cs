387bd77e9ff9a5cd15c38b9c171124950a161184
Accelwolf01 <accelwolf01@gmail.com>
2025-11-26 12:40:02 -0500

    Base de datos

--- obj/Debug/net9.0/back_tienda.AssemblyInfo.cs
+++ obj/Debug/net9.0/back_tienda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_tienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8c793cd23ba6bc273e47f7c39b8d4e094a565b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be47014dd062000cf229836bb056cec45678a5a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_tienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_tienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
